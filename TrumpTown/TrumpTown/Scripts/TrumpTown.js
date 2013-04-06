999f5808908abe99a7e56099c2d4964dcfb255c6
Kieranties <Kieran@Kieranties.com>
2013-04-06 15:02:42 +0100

    Stubbed logic for sending/recieving messages between client and server

---
+++ TrumpTown/TrumpTown/Scripts/TrumpTown.js
@@ -0,0 +1,41 @@
+﻿$(function () {
+    // Declare a proxy to reference the hub. 
+    var trumpTown = $.connection.trumpTownHub;
+
+    trumpTown.client.OnJoined = function (name) {
+        //Add username to list of people
+    };
+
+    trumpTown.client.OnPlayerReady = function (name) {
+        //highlght user as ready to play
+    };
+
+    trumpTown.client.OnDeal = function () {
+        // render the details of the card
+        var mycard = trumpTown.server.Deal();
+    };
+
+    trumpTown.client.OnEndRound = function (cardId, user) {
+        // if card and user match highlight as winner
+
+        // enable ready button
+    };
+
+    // Start the connection.
+    $.connection.hub.start().done(function () {
+        // do registration of events here
+        //$('#sendmessage').click(function () {
+        //    // Call the Send method on the hub. 
+        //    chat.server.send($('#displayname').val(), $('#message').val());
+        //    // Clear text box and reset focus for next comment. 
+        //    $('#message').val('').focus();
+        //});
+
+        //server calls to use in the right place
+        trumpTown.server.JoinGame("username");
+
+        trumpTown.server.Play("dataField", "dataValue", "higher/lower");
+
+        trumpTown.server.OnPlayerReady();
+    });
+});
--- TrumpTown/TrumpTown/TrumpTownHub.cs
+++ TrumpTown/TrumpTown/TrumpTownHub.cs
@@ -1,22 +1,74 @@
-﻿using Microsoft.AspNet.SignalR;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace TrumpTown
 {
     public class TrumpTownHub : Hub
     {
-        public void Hello()
+        private List<string> _clients = new List<string>();
+        private List<HubConnectionContext> _readyUsers;
+        private Dictionary<string, string> _cardsInPlay = new Dictionary<string, string>(); 
+        private DataAccess.MongoData _mongo = new DataAccess.MongoData();
+ 
+        public void JoinGame(string username)
         {
-            Clients.All.hello();
+            Clients.Caller.username = username;
+            Clients.Others.OnJoined(username);
         }
 
-        public void Send(string name, string message)
+        public override System.Threading.Tasks.Task OnDisconnected()
         {
-            TrumpTown.DataAccess.MongoData mongo = new TrumpTown.DataAccess.MongoData();
+            Clients.Others.OnLeave(Clients.Caller.Username);
+            _clients.Remove(Context.ConnectionId);
+            return base.OnDisconnected();
+        }
+        
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            _clients.Add(Context.ConnectionId);
+ 	         return base.OnConnected();
+        }
 
-            var a = mongo.GetRecord("515ff7a54f8165619b36efcb");
+        public string Deal()
+        {
+            var card = new Random().Next().ToString();
+            
+            var a = _mongo.GetRecord("515ff7a54f8165619b36efcb");
+            _cardsInPlay.Add(a.Id.ToString(), Context.ConnectionId);
 
-            // Call the broadcastMessage method to update clients.
-            Clients.All.broadcastMessage(name, a.GorName);
+            return card;
+        }
+
+        public void Play(string dataField, string dataValue, bool compareLower)
+        {
+            if (Clients.Caller.WonLast)
+            {
+                // compare data, get winner
+                var winningCard = Compare(dataField, dataValue, compareLower);
+
+                var winner = Clients.Client(_cardsInPlay[winningCard]);
+
+                winner.WonLast = true;
+                Clients.All.OnEndRound(winningCard, winner.Username);
+            }
+        }
+
+        private string Compare(string dataField, string dataValue, bool compareLower)
+        {
+            return null;
+        }
+        
+        public void PlayerReady()
+        {
+            _readyUsers.Add(Clients.Caller);
+            Clients.Others.OnPlayerReady(Clients.Caller.Username);
+
+            if (_readyUsers.Count.Equals(_clients.Count))
+                Clients.All.OnDeal();
+
+            _readyUsers.Clear();
         }
     }
 }
