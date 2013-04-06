c57875cf0730d98b62ff88d38db66258e00b063a
Adam Simmonds <adamsimsy@gmail.com>
2013-04-06 16:07:05 +0100

    Added example card markup and javascript for submit

---
+++ TrumpTown/TrumpTown/Scripts/trump-cards.js
@@ -0,0 +1,30 @@
+﻿$().ready(function () {
+    $("tr:odd").css("background-color", "#bbbbff");
+
+    $("#high").click(function () {
+        playTrumpCard("high");
+    });
+
+    $("#low").click(function () {
+        playTrumpCard("low");
+    });
+});
+
+
+
+function playTrumpCard(selection) {
+
+    var selected = $('input[name=trump-radio]:checked').val();
+
+
+    if (selected == undefined) {
+        $("#category-error").html("<p><strong>Please select a category!</strong></p>");
+    }
+    else {
+        var selectedCategory = $('label[for=' + selected + ']').text();
+
+        $("#category-played").html("<p>You played " + selectedCategory + " as " + selection + "!");
+        $("#category-highlow").css("display", "none");
+        $('input[name=trump-radio]').attr('disabled', true);
+    }
+}
